96d74e5b3dccb8559cc52643b897f243a3e0eef8
roshk99 <roshni.s.kaushik@gmail.com>
2023-02-17 15:47:07 -0500

    Added robot sound and body_face publisher in exercise session

--- src/quori_exercises/scripts/smach_test.py
+++ src/quori_exercises/scripts/smach_test.py
@@ -3,7 +3,7 @@ import roslib
 import rospy
 import smach
 import smach_ros
-
+from std_msgs.msg import Float64MultiArray, String
 
 class Start(smach.State):
     def __init__(self):
@@ -57,7 +57,7 @@ class ExerciseGroup(smach.State):
         rospy.loginfo('Executing state EXERCISEGROUP')
         for exercise_index, (exercise, sets_complete) in enumerate(zip(userdata.sm_exercise_order, userdata.sm_set_counter)):
             if sets_complete < userdata.sm_sets_per_exercise:
-                userdata.sm_current_exercise = [exercise_index, exercise]
+                userdata.sm_current_exercise = [exercise_index, exercise, sets_complete+1]
                 return 'begin_exercise'
             else:
                 rospy.loginfo('Exercise {} complete'.format(exercise))
@@ -67,17 +67,68 @@ class ExerciseGroup(smach.State):
 
 class Exercise(smach.State):
     def __init__(self):
-        smach.State.__init__(self, outcomes=['end_exercise'],
-                                    input_keys=['sm_current_exercise', 'sm_set_counter'],
+        smach.State.__init__(self, outcomes=['begin_rest'],
+                                    input_keys=['sm_current_exercise', 'sm_set_counter', 'sm_set_counter', 'sm_sets_per_exercise', 'sm_exercise_time'],
                                     output_keys=['sm_set_counter'])
+        self.quori_body_face_pub = rospy.Publisher("quori_body_face", Float64MultiArray, queue_size=10)
+        self.quori_sound_pub = rospy.Publisher('quori_sound', String, queue_size=10)
 
     def execute(self, userdata):
-        rospy.loginfo('Executing state EXERCISE {} - {}'.format(userdata.sm_current_exercise[0], userdata.sm_current_exercise[1]))
-        # sleep for duration
-        d = rospy.Duration(1, 0)
+        rospy.loginfo('Executing state EXERCISE {}/{} ({}) - Set {}/{}'.format(userdata.sm_current_exercise[0]+1, 
+                                                                            len(userdata.sm_set_counter), 
+                                                                            userdata.sm_current_exercise[1],
+                                                                            userdata.sm_current_exercise[2],
+                                                                            userdata.sm_sets_per_exercise))
+                
+        last_message = rospy.get_rostime()
+        robot_message = "Time to do some %s" % userdata.sm_current_exercise[1]
+        rospy.loginfo("Robot will say: {}".format(robot_message))
+        self.quori_sound_pub.publish(robot_message)
+        
+        start = rospy.get_rostime()
+        d = rospy.Duration(userdata.sm_exercise_time, 0)
+
+        while start + d > rospy.get_rostime():
+            if last_message + rospy.Duration(4, 0) < rospy.get_rostime():
+                last_message = rospy.get_rostime()
+
+                robot_message = "Great job with your %s" % userdata.sm_current_exercise[1]
+                rospy.loginfo("Robot will say: {}".format(robot_message))
+                self.quori_sound_pub.publish(robot_message)
+
+                body_face_msg = Float64MultiArray()
+                body_face_msg.data = [0, 1, 2]
+                rospy.loginfo("Robot will move: {}".format(body_face_msg.data))
+                self.quori_body_face_pub.publish(body_face_msg)
+
+        return 'begin_rest'
+
+class Rest(smach.State):
+    def __init__(self):
+        smach.State.__init__(self, outcomes=['end_exercise'],
+                                    input_keys=['sm_current_exercise', 'sm_set_counter', 'sm_rest_between_sets', 'sm_rest_between_exercises', 'sm_sets_per_exercise'],
+                                    output_keys=['sm_current_exercise'])
+
+    def execute(self, userdata):
+        rospy.loginfo('Executing state EXERCISE {}/{} ({}) - Set {}/{}'.format(userdata.sm_current_exercise[0]+1, 
+                                                                            len(userdata.sm_set_counter), 
+                                                                            userdata.sm_current_exercise[1],
+                                                                            userdata.sm_current_exercise[2],
+                                                                            userdata.sm_sets_per_exercise))
+
+        #Check if last set in exercise
+        if userdata.sm_set_counter[userdata.sm_current_exercise[0]] == userdata.sm_sets_per_exercise - 1:
+            rest_time = userdata.sm_rest_between_exercises
+        else:
+            rest_time = userdata.sm_rest_between_sets
+        
+        d = rospy.Duration(rest_time, 0)
+        rospy.loginfo('Resting for {} sec'.format(rest_time))
         rospy.sleep(d)
         userdata.sm_set_counter[userdata.sm_current_exercise[0]] += 1
         return 'end_exercise'
+
+
 
 
 #Start with this
@@ -88,12 +139,13 @@ def main():
 
     # Create the top level SMACH state machine
     sm_main = smach.StateMachine(outcomes=['complete'])
-    sm_main.userdata.sm_exercise_order = ['bicep_curls', 'lateral_raises']
+    sm_main.userdata.sm_exercise_order = ['bicep curls', 'lateral raises']
     sm_main.userdata.sm_set_counter = [0, 0]
     sm_main.userdata.sm_sets_per_exercise = 3
-    sm_main.userdata.sm_rest_between_sets = 30
-    sm_main.userdata.sm_rest_between_exercises = 45
-    sm_main.userdata.sm_current_exercise = [-1, '']
+    sm_main.userdata.sm_rest_between_sets = 3
+    sm_main.userdata.sm_rest_between_exercises = 4
+    sm_main.userdata.sm_exercise_time = 15
+    sm_main.userdata.sm_current_exercise = [-1, '', -1] #Exercise number, exercise name, set number
 
     # Open the container
     with sm_main:
@@ -111,7 +163,11 @@ def main():
                                transitions={'continue_study':'complete', 'begin_exercise': 'EXERCISE'})
 
         smach.StateMachine.add('EXERCISE', Exercise(),
+                               transitions={'begin_rest': 'REST'})
+        
+        smach.StateMachine.add('REST', Rest(),
                                transitions={'end_exercise': 'EXERCISEGROUP'})
+
        
     # Create and start the introspection server
     sis = smach_ros.IntrospectionServer('server_name', sm_main, '/')
