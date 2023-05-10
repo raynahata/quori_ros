003992d62cba97e79327a07156ac7d8a3aa3f127
Roshni <roshni.s.kaushik@gmail.com>
2023-05-10 12:38:58 -0400

    Tuning

--- src/quori_exercises/scripts/exercise_session.py
+++ src/quori_exercises/scripts/exercise_session.py
@@ -12,9 +12,8 @@ from scipy.spatial.distance import euclidean
 import matplotlib.pyplot as plt
 import logging
 import sys
-import subprocess, shlex, psutil
 
-PARTICIPANT_ID = 1
+PARTICIPANT_ID = 'A'
 ROUND_NUM = 1
 ROBOT_NUM = 1
 EXERCISE_LENGTH = 30
@@ -50,10 +49,10 @@ class ExerciseEval:
     def check_if_new_peak(self, grads, peak_candidate, index_to_search):
         #Peaks in absolute indices
         #Grads, peak_candidates in relative units
-
+        
         if (len(self.peaks) == 0 and index_to_search[peak_candidate] > 15) or (len(self.peaks) > 0 and self.peaks[-1] + 15 < index_to_search[peak_candidate]):
             range_to_check = np.arange(np.max([peak_candidate-4, 0]), np.min([peak_candidate+4, grads.shape[0]])).astype('int')
-
+            
             max_val = np.argmax(grads[range_to_check,:][:,self.segmenting_joints],axis=0)
             max_val = np.mean(max_val).astype('int')
 
@@ -72,12 +71,12 @@ class ExerciseEval:
             actual_max_loc = np.where(self.angles[range_to_check][:,self.segmenting_joints] == actual_max)[0][0]
             actual_min_loc = np.where(self.angles[range_to_check][:,self.segmenting_joints] == actual_min)[0][0]
 
-            # print(index_to_search[range_to_check[max_val]], 'Grad Max:', actual_grad_max, 'Grad Min:', actual_grad_min, 'Max Loc:', actual_max_loc, 'Min Loc:', actual_min_loc)
+            # print(index_to_search[range_to_check[max_val]], 'Grad Max:', actual_grad_max, 'Grad Min:', actual_grad_min, 'Max Loc:', actual_max_loc, 'Min Loc:', actual_min_loc, 'Actual Max:', actual_max, 'Actual Min:', actual_min)
             if (actual_grad_max > grad_max \
                 or actual_grad_min < grad_min) \
                     and actual_max_loc > actual_min_loc:
                 
-                if (self.exercise_name == 'bicep_curls' and actual_min < 50 and actual_max > 100) or self.exercise_name == 'lateral_raises':
+                if (self.exercise_name == 'bicep_curls' and actual_min < 50 and actual_max > 100) or (self.exercise_name == 'lateral_raises' and actual_max > 100 and actual_min < 50):
                     # print('added')
                     return range_to_check[max_val]
             
@@ -120,7 +119,7 @@ class ExerciseEval:
                 threshold1 = self.threshold1[1]
                 threshold2 = self.threshold2[1]
             good_distances = [expert_distances[ii] for ii in self.good_experts]
-            
+
             if np.min(good_distances) < threshold1:
                 closest_expert = self.good_experts[np.argmin(good_distances)]
                 best_distance = np.min(good_distances)
@@ -129,7 +128,7 @@ class ExerciseEval:
                 best_distance = np.min(expert_distances)
             expert_label = self.labels[closest_expert]
             
-            logger.info('{} - Closest good expert {}, Closest All expert {}, Closest expert label {}'.format(joint_group, np.min(good_distances), np.min(expert_distances), self.labels[np.argmin(expert_distances)]))
+            # logger.info('{} - Closest good expert {}, Closest All expert {}, Closest expert label {}'.format(joint_group, np.min(good_distances), np.min(expert_distances), self.labels[np.argmin(expert_distances)]))
             
             if best_distance < threshold1:
                 correction = expert_label
@@ -145,14 +144,13 @@ class ExerciseEval:
             corrections.append(correction)
 
         
-        if rep_duration < np.mean(self.expert_duration) + 3*np.std(self.expert_duration):
+        if rep_duration < np.mean(self.expert_duration) - 2:
             speed = 'fast'
-        elif rep_duration > np.mean(self.expert_duration) - 3*np.std(self.expert_duration):
+        elif rep_duration > np.mean(self.expert_duration) + 2:
             speed = 'slow'
         else:
             speed = 'good'
             
-        
         feedback = {'speed': speed, 'correction': corrections}
         logger.info(feedback)
         return feedback
@@ -174,7 +172,8 @@ class ExerciseEval:
 
         #Read angle from message
         angle = angle_data.data
-        self.angles = np.vstack((self.angles, angle))
+        # print(np.array(angle).shape, self.angles.shape)
+        self.angles = np.vstack((self.angles, np.array(angle)))
 
         #Get time
         time = datetime.now(tz)
@@ -204,11 +203,11 @@ class ExerciseEval:
     def reeval(self):
         for index, angle in enumerate(self.angles):
             #Look for new peaks
-            if index % 10 == 0 and index > 15:
-                index_to_search = np.arange(np.max([0,index-500]), index).astype('int')
+            if index > 15:
+                index_to_search = np.arange(0, index).astype('int')
                 
                 peak_candidates, grads  = self.find_peaks(self.angles[index_to_search,:])
-                    
+  
                 #Get actual list of peaks
                 for peak_candidate in peak_candidates:
                     res = self.check_if_new_peak(grads, peak_candidate, index_to_search)
@@ -221,16 +220,6 @@ class ExerciseEval:
                             rep_duration = (self.times[self.peaks[-1]] - self.times[self.peaks[-2]]).total_seconds()
                             feedback = self.evaluate_rep(current_rep, rep_duration)
                             self.feedback.append(feedback)
-        
-        if len(self.peaks) > 0 and self.peaks[-1] + 20 < self.angles.shape[0]:
-            #Add last point
-            self.peaks.append(self.angles.shape[0]-1)
-
-            #Evaluate rep
-            current_rep = self.angles[self.peaks[-2]:self.peaks[-1],:]
-            rep_duration = (self.times[self.peaks[-2]] - self.times[self.peaks[-1]]).total_seconds()
-            feedback = self.evaluate_rep(current_rep, rep_duration)
-            self.feedback.append(feedback)
 
     def face_callback(self, face_data):
         if not self.flag:
@@ -254,26 +243,27 @@ class ExerciseEval:
                     style = '--'
                 else:
                     style = '-'
-                correction = self.feedback[counter]['correction'][self.joint_to_groups[ii]]
-                if 'Good' in correction:
-                    color = 'g'
-                elif 'ok' in correction:
-                    color = 'y'
-                elif 'bad' in correction:
-                    color = 'r'
-                elif 'low' in correction:
-                    color = 'm'
-                elif 'high' in correction:
-                    color = 'b'
-                else:
-                    color = 'k' 
+                if counter < len(self.feedback):
+                    correction = self.feedback[counter]['correction'][self.joint_to_groups[ii]]
+                    if 'Good' in correction:
+                        color = 'g'
+                    elif 'ok' in correction:
+                        color = 'y'
+                    elif 'bad' in correction:
+                        color = 'r'
+                    elif 'low' in correction:
+                        color = 'm'
+                    elif 'high' in correction:
+                        color = 'b'
+                    else:
+                        color = 'k' 
 
-                ax[ii].plot(np.arange(start, end), self.angles[start:end,ii], linestyle=style, color=color)
+                    ax[ii].plot(np.arange(start, end), self.angles[start:end,ii], linestyle=style, color=color)
                 
 
                 counter += 1
 
-            ax[ii].set_title('{}-{}-{}-{}'.format(self.joints[ii][0], self.joints[ii][1], self.joints[ii][2], self.joints[ii][3]))
+            ax[ii].set_title('{}-{}-{}-{}-{}'.format(ii,self.joints[ii][0], self.joints[ii][1], self.joints[ii][2], self.joints[ii][3]), fontsize = 8)
         plt.show()
 
 def from_file(filename, re_eval):
@@ -287,6 +277,9 @@ def from_file(filename, re_eval):
     npzfile = np.load('/home/quori4/quori_files/quori_ros/src/quori_exercises/experts/{}_experts.npz'.format(exercise_eval.exercise_name), allow_pickle=True)
     
     exercise_eval.joints, exercise_eval.experts, exercise_eval.expert_duration, exercise_eval.segmenting_joints, exercise_eval.labels = npzfile['joints'], npzfile['experts'], npzfile['expert_duration'], npzfile['segmenting_joints'], npzfile['labels']
+
+    if exercise_eval.exercise_name == 'lateral_raises':
+        exercise_eval.segmenting_joints = [0] + exercise_eval.segmenting_joints
 
     exercise_eval.angles = data_file['angles']
     exercise_eval.facial_features = data_file['facial_features']
@@ -321,10 +314,10 @@ if __name__ == '__main__':
     #Initialize exercise evaluation variables
     tz = timezone('EST')
 
-    from_file_flag = False
-    re_eval = False
+    from_file_flag = True
+    re_eval = True
     SET_NUM = 1
-    EXERCISE_NAME = 'bicep_curls'
+    EXERCISE_NAME = 'lateral_raises'
 
     data_filename = 'Participant_{}_Round_{}_Robot_{}_Exercise_{}_Set_{}.npz'.format(PARTICIPANT_ID, ROUND_NUM, ROBOT_NUM, EXERCISE_NAME, SET_NUM)
     bag_filename = 'Participant_{}_Round_{}_Robot_{}_Exercise_{}_Set_{}.bag'.format(PARTICIPANT_ID, ROUND_NUM, ROBOT_NUM, EXERCISE_NAME, SET_NUM)
@@ -376,6 +369,9 @@ if __name__ == '__main__':
                 exercise_eval.joints, exercise_eval.experts, exercise_eval.expert_duration, exercise_eval.segmenting_joints, exercise_eval.labels = npzfile['joints'], npzfile['experts'], npzfile['expert_duration'], npzfile['segmenting_joints'], npzfile['labels']
 
                 exercise_eval.good_experts = np.array([ii for ii, label in enumerate(exercise_eval.labels) if 'Good' in label]).astype(int)
+
+                if exercise_eval.exercise_name == 'lateral_raises':
+                    exercise_eval.segmenting_joints = [0] + exercise_eval.segmenting_joints
 
                 exercise_eval.angles = np.empty((0,len(exercise_eval.joints)))
                 exercise_eval.facial_features = np.empty((0,7))
